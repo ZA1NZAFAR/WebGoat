b20772ce9ed7325ef50027e3ece29dc8ee74ada6
za1nzafar <zainzafar@outlook.fr>
2024-03-24 20:41:11 +0100

    fix sql inj

--- src/main/java/org/owasp/webgoat/lessons/sqlinjection/introduction/SqlInjectionLesson9.java
+++ src/main/java/org/owasp/webgoat/lessons/sqlinjection/introduction/SqlInjectionLesson9.java
@@ -18,81 +18,79 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @AssignmentHints(
-        value = {
-                "SqlStringInjectionHint.9.1",
-                "SqlStringInjectionHint.9.2",
-                "SqlStringInjectionHint.9.3",
-                "SqlStringInjectionHint.9.4",
-                "SqlStringInjectionHint.9.5"
-        })
+    value = {
+      "SqlStringInjectionHint.9.1",
+      "SqlStringInjectionHint.9.2",
+      "SqlStringInjectionHint.9.3",
+      "SqlStringInjectionHint.9.4",
+      "SqlStringInjectionHint.9.5"
+    })
 public class SqlInjectionLesson9 extends AssignmentEndpoint {
 
-    private final LessonDataSource dataSource;
+  private final LessonDataSource dataSource;
 
-    public SqlInjectionLesson9(LessonDataSource dataSource) {
-        this.dataSource = dataSource;
-    }
+  public SqlInjectionLesson9(LessonDataSource dataSource) {
+    this.dataSource = dataSource;
+  }
 
-    @PostMapping("/SqlInjection/attack9")
-    @ResponseBody
-    public AttackResult completed(@RequestParam String name, @RequestParam String auth_tan) {
-        return injectableQueryIntegrity(name, auth_tan);
-    }
+  @PostMapping("/SqlInjection/attack9")
+  @ResponseBody
+  public AttackResult completed(@RequestParam String name, @RequestParam String auth_tan) {
+    return injectableQueryIntegrity(name, auth_tan);
+  }
 
-    protected AttackResult injectableQueryIntegrity(String name, String auth_tan) {
-        StringBuilder output = new StringBuilder();
-        String query =
-                "SELECT * FROM employees WHERE last_name = ? AND auth_tan = ?";
-        try (Connection connection = dataSource.getConnection()) {
-            try (PreparedStatement statement = connection.prepareStatement(query, TYPE_SCROLL_SENSITIVE, CONCUR_UPDATABLE)) {
-                statement.setString(1, name);
-                statement.setString(2, auth_tan);
-                SqlInjectionLesson8.log(connection, query); // Note: logging placeholder query
-                ResultSet results = statement.executeQuery();
-                if (results.getStatement() != null) {
-                    if (results.first()) {
-                        output.append(SqlInjectionLesson8.generateTable(results));
-                    } else {
-                        // no results
-                        return failed(this).feedback("sql-injection.8.no.results").build();
-                    }
-                }
-            } catch (SQLException e) {
-                System.err.println(e.getMessage());
-                return failed(this)
-                        .output("<br><span class='feedback-negative'>" + e.getMessage() + "</span>")
-                        .build();
-            }
-
-            return checkSalaryRanking(connection, output);
-
-        } catch (Exception e) {
-            System.err.println(e.getMessage());
-            return failed(this)
-                    .output("<br><span class='feedback-negative'>" + e.getMessage() + "</span>")
-                    .build();
+  protected AttackResult injectableQueryIntegrity(String name, String auth_tan) {
+    StringBuilder output = new StringBuilder();
+    String query = "SELECT * FROM employees WHERE last_name = ? AND auth_tan = ?";
+    try (Connection connection = dataSource.getConnection()) {
+      try (PreparedStatement statement =
+          connection.prepareStatement(query, TYPE_SCROLL_SENSITIVE, CONCUR_UPDATABLE)) {
+        statement.setString(1, name);
+        statement.setString(2, auth_tan);
+        SqlInjectionLesson8.log(connection, query); // Note: logging placeholder query
+        ResultSet results = statement.executeQuery();
+        if (results.getStatement() != null) {
+          if (results.first()) {
+            output.append(SqlInjectionLesson8.generateTable(results));
+          } else {
+            // no results
+            return failed(this).feedback("sql-injection.8.no.results").build();
+          }
         }
-    }
+      } catch (SQLException e) {
+        System.err.println(e.getMessage());
+        return failed(this)
+            .output("<br><span class='feedback-negative'>" + e.getMessage() + "</span>")
+            .build();
+      }
 
-    private AttackResult checkSalaryRanking(Connection connection, StringBuilder output) {
-        String query = "SELECT * FROM employees ORDER BY salary DESC";
-        try (PreparedStatement statement = connection.prepareStatement(query, TYPE_SCROLL_SENSITIVE, CONCUR_UPDATABLE)) {
-            ResultSet results = statement.executeQuery();
-            results.first();
-            // user completes lesson if John Smith is the first in the list
-            if ((results.getString(2).equals("John")) && (results.getString(3).equals("Smith"))) {
-                output.append(SqlInjectionLesson8.generateTable(results));
-                return success(this)
-                        .feedback("sql-injection.9.success")
-                        .output(output.toString())
-                        .build();
-            } else {
-                return failed(this).feedback("sql-injection.9.one").output(output.toString()).build();
-            }
-        } catch (SQLException e) {
-            return failed(this)
-                    .output("<br><span class='feedback-negative'>" + e.getMessage() + "</span>")
-                    .build();
-        }
+      return checkSalaryRanking(connection, output);
+
+    } catch (Exception e) {
+      System.err.println(e.getMessage());
+      return failed(this)
+          .output("<br><span class='feedback-negative'>" + e.getMessage() + "</span>")
+          .build();
     }
+  }
+
+  private AttackResult checkSalaryRanking(Connection connection, StringBuilder output) {
+    String query = "SELECT * FROM employees ORDER BY salary DESC";
+    try (PreparedStatement statement =
+        connection.prepareStatement(query, TYPE_SCROLL_SENSITIVE, CONCUR_UPDATABLE)) {
+      ResultSet results = statement.executeQuery();
+      results.first();
+      // user completes lesson if John Smith is the first in the list
+      if ((results.getString(2).equals("John")) && (results.getString(3).equals("Smith"))) {
+        output.append(SqlInjectionLesson8.generateTable(results));
+        return success(this).feedback("sql-injection.9.success").output(output.toString()).build();
+      } else {
+        return failed(this).feedback("sql-injection.9.one").output(output.toString()).build();
+      }
+    } catch (SQLException e) {
+      return failed(this)
+          .output("<br><span class='feedback-negative'>" + e.getMessage() + "</span>")
+          .build();
+    }
+  }
 }
